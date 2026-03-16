54a346bf4e5c93bd38479824bbd8c7a01f49f77d
EugZ236 <zhmenka2003@gmail.com>
2026-03-16 14:07:56 +0200

    multicast

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,59 @@
 import { Tabs } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { HapticTab } from "@/components/haptic-tab";
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
+import messaging from "@react-native-firebase/messaging";
+import * as Notifications from "expo-notifications";
+
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    // 2. Создание канала специально для Android
+    async function configureAndroidChannel() {
+      if (Platform.OS === "android") {
+        await Notifications.setNotificationChannelAsync("default", {
+          name: "Default",
+          importance: Notifications.AndroidImportance.MAX, // Максимальный приоритет для баннера
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+    }
+    configureAndroidChannel();
+
+    // 3. Прослушивание сообщений от Firebase
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      console.log("FCM Message received in foreground:", remoteMessage);
+
+      // 4. Ручной запуск локального уведомления для показа баннера
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: remoteMessage.notification?.title || "Заголовок",
+          body: remoteMessage.notification?.body || "Текст сообщения",
+          data: remoteMessage.data, // передаем данные для обработки нажатия
+        },
+        trigger: null, // Показать мгновенно
+      });
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <Tabs
--- app/login.tsx
+++ app/login.tsx
@@ -16,7 +16,7 @@ import {
 } from "react-native";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("Test@gmail.com");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("Test12345!");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
