c8cf63f434ec57e22576faebb8a61dbdbbab5607
rilking1 <kozakevycr2@gmail.com>
2026-03-24 17:12:37 +0200

    feat: setup EAS build for Android APK and integrate Azure backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 const api = axios.create({
-  baseURL: "http://192.168.31.150:5000",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
